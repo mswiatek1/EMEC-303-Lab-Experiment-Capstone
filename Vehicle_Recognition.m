ee3fd753da52922c618085f0849cd32f1484b7b1
mswiatek1 <matthew.swiatek@gmail.com>
2016-03-05 17:02:02 -0700

    Here are two different codes for recognizing the car/ moving object.  Vehicle_Recognition is the original version from last week, updated to work on the latest version.  Vehicle_Recognition_ver_2 is an improved code, also simpler I think.  There are also two more videos to use for testing.

--- Vehicle_Recognition.m
+++ Vehicle_Recognition.m
@@ -15,36 +15,40 @@ end
 
 % Open Video, Collect info from the file
 vid = VideoReader(filename);
-read(vid, Inf);
-LastFrame = vid.NumberOfFrames; % Total number of frames in the video
+v= readFrame(vid);
+LastTime = vid.Duration; % Total number of frames in the video
 vidHeight = vid.Height; % Height in Pixels
 vidWidth = vid.Width; % Width in Pixels
-
+LastFrame = floor(vid.Duration*vid.FrameRate);
 NumberFramesWritten = 0;
-
+FirstTime = LastTime/vid.FrameRate;
 % Convert video to black and white binary
 BWvid = zeros(vidHeight,vidWidth,LastFrame); % Preallocate array
-ObjMotVid = zeros(vidHeight,vidWidth,LastFrame); % Preallocate array
-for i=1:LastFrame
-    currentFrame = read(vid,i);
+BW2vid = zeros(vidHeight,vidWidth,LastFrame); % Preallocate array
+diff = zeros(vidHeight,vidWidth,LastFrame);
+for i=1:LastFrame-1
+    currentFrame = readFrame(vid);
     level = graythresh(currentFrame); % Threshold level for black/ white
     BWvid(:,:,i) = im2bw(currentFrame,level); % Convert image to black and white (binary)
-    %ObjMotVid(:,:,i) = BWvid(:,:,i) - BWvid(:,:,1); % Subtract first frame from video
-    %BWcurrentFrame = im2bw(currentFrame,level);
-%     A = 0.5;
-%     if i == 1
-%         Bkgd = currentFrame;
-%     else
-%         Bkgd = (1-A)*currentFrame+A*Bkgd;
-%     end
-    % Convert Bkgd to bw
-    %BWBkgd = im2bw(Bkgd,level);
-    %imshow(Bkgd)
     
-    %diff = BWcurrentFrame - Bkgd;
+    BW2vid(:,:,i) = bwareaopen(BWvid(:,:,i),1000);
+
+    diff(:,:,i) = imabsdiff(BW2vid(:,:,i),BW2vid(:,:,1));
+   
+%    s = regionprops(logical(diff(:,:,i)),'area','centroid');
+%    centroids = cat(1, s.Centroid);
+
+   
+   
 end
- % diff = BWvid(:,:,i)-BWBkgd;
-%implay(BWvid)
-%implay(ObjMotVid)
-%implay(Diff)
+
+%implay(diff)
+
+% t=1:LastFrame;
+% subplot(2, 1, 1)
+% plot(t, centroids(:,1)), ylabel('x')
+% subplot(2, 1, 2)
+% plot(t, centroids(:, 2)), ylabel('y')
+% xlabel('time (s)')
+
 
