074f2402a16db66d8be4b567c5a83d2c0891a6b8
mswiatek1 <matthew.swiatek@gmail.com>
2016-03-29 21:52:10 -0600

    More updates to tracking code

--- Color_Tracking.m
+++ Color_Tracking.m
@@ -20,49 +20,80 @@ vid.CurrentTime = LastTime/2;
 LastFrame = floor(LastTime*vid.FrameRate);
 mid = readFrame(vid); % Reads the frame at the middle timestep in video for analyzing
 
+
+figure(1); clf(1);
+imshow(mid);
+pause
+hold on
 readpix = impixel(mid);
+hold off
 red = readpix(1); % Red value
 green = readpix(2); % Green Value
 blue = readpix(3); % Blue value
 
 %
-p = 0.2;
+p = 0.15;
 rdiff = p*red; % Red
 gdiff = p*green; % Green
 bdiff = p*blue; % Blue
-vid.CurrentTime = LastTime/2; % Rewind
-LastFrame=floor(LastFrame/10);
+vid.CurrentTime = 0; % Rewind
+FirstFrame=readFrame(vid); % First Frame as a background
 trackedObj = zeros(vidHeight,vidWidth,LastFrame);
+newrgb = zeros(vidHeight,vidWidth,LastFrame);
 [m, n, q] = size(trackedObj);
-for i = 1:LastFrame-1
-    currentFrame = readFrame(vid);
+
+for i = 1:q-1
+    currentFrame = readFrame(vid); %- FirstFrame;
     redchan = currentFrame(:,:,1); % Red Channel
     greenchan = currentFrame(:,:,2); % Green Channel
     bluechan = currentFrame(:,:,3); % Blue Channel
-    
-    for j = 1:m
-        for k = 1:n
-            if (redchan(j,k) <= red+rdiff && redchan(j,k) >= red-rdiff && ...
-                greenchan(j,k) <= green+gdiff && greenchan(j,k) >= green-gdiff && ...     
-                bluechan(j,k) <= blue+bdiff && bluechan(j,k) >= blue-bdiff)
-
-                trackedObj(j,k,i) = 1;
-            else
-                trackedObj(j,k,i) = 0;
+    i
+    % This loop finds black pixels and makes them similar to the board
+    for jj = 1:m
+        for kk = 1:n
+%             redpix = redchan(j,k,i);
+%             greenpix = greenchan(j,k,i);
+%             bluepix = bluechan(j,k,i);
+            if (redchan(jj,kk) <= 50 && greenchan(jj,kk) <= 50 && bluechan(jj,kk) <= 50)
+                redchan(jj,kk,i) = 140;
+                greenchan(jj,kk,i) = 140;
+                bluechan(jj,kk,i) = 130;
             end
         end
     end
-    
-    figure(1); clf(1)
-    subplot(2,1,1)
-    imshow(currentFrame)
-    subplot(2,1,2)
-    imshow(trackedObj(:,:,i))
-    pause
-    
+    newrgb = cat(3,redchan,greenchan,bluechan);
 end
-        
-        
-        
-        
-        
+% 
+% for i = 1:q-1
+%     
+%     imshow(newrgb)
+%     redch = 
+%     % This loop finds the pixels within the range of the selected color and
+%     % marks them as a point if they are within the range.  If not, it is
+%     % set as zero
+%     for j = 1:m
+%         for k = 1:n
+%             if (redchan(j,k) <= red+rdiff && redchan(j,k) >= red-rdiff && ...
+%                 greenchan(j,k) <= green+gdiff && greenchan(j,k) >= green-gdiff && ...     
+%                 bluechan(j,k) <= blue+bdiff && bluechan(j,k) >= blue-bdiff)
+% 
+%                 trackedObj(j,k,i) = 1;
+%             else
+%                 trackedObj(j,k,i) = 0;
+%             end
+%         end
+%     end
+%     
+%     figure(2); clf(2)
+%     subplot(2,1,1)
+%     imshow(currentFrame)
+%     subplot(2,1,2)
+%     imshow(trackedObj(:,:,i))
+%     pause
+%     
+% end
+%         
+%         
+%         
+%         
+%         
