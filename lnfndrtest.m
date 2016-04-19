d96c6900900acf7dbc34efe80689b4937185b092
mswiatek1 <matthew.swiatek@gmail.com>
2016-04-18 20:39:19 -0600

    Here are the most updated codes for finding the track shape and tracking the vehicle (in the shiny and polished folder).  Both of these have been cleaned up to remove unnecessary code/ portions that have been commented out.  The Track Shape code also has the calibration portion in it now.

--- lnfndrtest.m
+++ lnfndrtest.m
@@ -8,6 +8,70 @@ else
     fprintf('%s was selected \n',filename)
 end
 
+
+% Open Image, Collect info from the file
+img = imread(filename);
+
+size(img);
+level = graythresh(img); % Threshold level for black/ white
+I = im2bw(img,level); % Convert image to black and white (binary)
+
+% Insert image into figure
+
+[rows, columns] = size(I);
+figure(1); clf(1);
+subplot(2,1,1);
+imshow(I); % Shows frame grab converted to black and white
+set(gcf, 'name','Spatial Calibration','numbertitle','off');
+
+units = 'pixels';
+Calibration = 1.0;
+button = 1;
+while button ~= 4
+    % User input of required action
+    button = menu('Pick One','Measure','Calibrate','Automatic A in Dr. Owkes Class','Proceed');
+    if button == 4 % EXIT
+        break; % End because user clicked exit
+    elseif button == 1 % Measure
+        %subplot(2,1,1);
+        title('Left Click First Point; Right Click Last Point.');
+        [x,y,profile]=improfile();
+        % Calculate Distance of line
+        dip = sqrt((x(1)-x(end))^2 + (y(1)-y(end))^2);
+        subplot(2,1,2);
+        plot(profile);
+        grid on;
+    elseif button == 2 % Calibration
+        Prompt = {'Enter True Size','Enter Units'};
+        defaultVals = {'12','Inches'};
+        UserInput = inputdlg(Prompt,'Enter a known distance',2,defaultVals);
+        if isempty(UserInput)
+            return
+        end
+        % Transfer user input into matlab
+        RealLength = str2double(UserInput{1});
+        units = char(UserInput{2});
+        % Check to make sure the number provided is a number
+        if isnan(RealLength)
+            warndlg('There is something wrong with the previous step. The pixel length will be used instead','Whoops! Something went wrong here!');
+            RealLength = dip;
+            units = 'pixels';
+            Calibration = 1.0; 
+            continue; % This skips to the end of the loop
+        end
+        Calibration = RealLength/dip;
+    elseif button == 3
+        errordlg('Sorry, the portion of code you are trying to access is no longer valid.  You will have to earn an A the hard way.','It was worth a shot!');
+    end
+    
+end
+    RealDistance = dip*Calibration;
+    caption = sprintf('Intensity Profile Along Line \nThe distance = %f pixels = %f %s', dip, RealDistance, units);
+    title(caption);
+    ylabel('Color');
+    xlabel('Pixels Along Line');
+
+figure(2); clf(2);
 bpoint=imread(filename);
 imshow(filename);
  uiwait(msgbox('Click a point left of the track THEN click a point right of the track','','modal'));
@@ -55,19 +119,48 @@ else
     end
 end
 
-
+A = zeros(m,n);
 x=1:n;
-for i=1:n
- a=bluecolor(:,i);
- bpoint=find(a);
-top=bpoint(1);
-bottom=bpoint(end);
- y(i)=((top+bottom)/2);
+for i = 1:n
+   A(:,i) = single(bluecolor(:,i));
+   s = regionprops(A(:,i),'centroid');
+    if max(bluecolor(:,i)) == 1 % if there is a centroid found store it in the following way
+        y(i)=s.Centroid(2); % checks x component
+    end
 end
-spy(bluecolor)
- 
+
+for i=1:n-1 % loop to find (0,0) centroids and readjust them through linear interpolation
+    if y(i) == 0
+       y(i) = NaN; % new y value
+    end
+end
+
+
+% %
+% for i=1:n
+%  a=bluecolor(:,i);
+%  bpoint=find(a);
+% top=bpoint(1);
+% bottom=bpoint(end);
+%  y(i)=((top+bottom)/2);
+% end
+% spy(bluecolor)
+% %
+
+
 %  [Ix,Iy]=find(bluecolor)
 
-figure(1)
-scatter(x,-y,'.')
-axis([0,n,-m,0]);
+% Convert from pixels to units with the calibration factor
+newx = x*Calibration;
+newy = -y*Calibration;
+newm = -m*Calibration;
+newn = n*Calibration;
+figure(3); clf(3);
+scatter(newx,newy,'.')
+axis([0,newn,newm,0]);
+title('Shape of the Track')
+xlabel(sprintf('%s', units))
+ylabel(sprintf('%s', units))
+
+
+
