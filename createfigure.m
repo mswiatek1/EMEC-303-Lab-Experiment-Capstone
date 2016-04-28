1dc972eacce8cc62b111788fc60b9eba2407473c
mhouser1 <mgh419@gmail.com>
2016-04-27 19:30:30 -0600

    Added files via upload

--- createfigure.m
+++ createfigure.m
@@ -34,7 +34,7 @@ end
 
 % Find x values for plotting the fit based on xlim
 axesLimits1 = xlim(axes1);
-xplot1 = linspace(axesLimits1(1), axesLimits1(2)-10);
+xplot1 = linspace(axesLimits1(1), axesLimits1(2)-5);
 % Preallocate for "Show equations" coefficients
 coeffs1 = cell(1,1);
 fitResults1 = polyfit(xdata1,ydata1,d);% Find coefficients for polynomial 
@@ -67,6 +67,80 @@ ylabel('Inches');
 
 
 legend(axes1,'show');
+
+function showEquations(fittypes1, coeffs1, digits1, axesh1)
+%SHOWEQUATIONS(FITTYPES1,COEFFS1,DIGITS1,AXESH1)
+%  Show equations
+%  FITTYPES1:  types of fits
+%  COEFFS1:  coefficients
+%  DIGITS1:  number of significant digits
+%  AXESH1:  axes
+
+n = length(fittypes1);
+txt = cell(length(n + 1) ,1);
+txt{1,:} = ' ';
+for i = 1:n
+    txt{i + 1,:} = getEquationString(fittypes1(i),coeffs1{i},digits1,axesh1);
+end
+text(.05,.95,txt,'parent',axesh1, ...
+    'verticalalignment','top','units','normalized');
+
+%-------------------------------------------------------------------------%
+function [s1] = getEquationString(fittype1, coeffs1, digits1, axesh1)
+%GETEQUATIONSTRING(FITTYPE1,COEFFS1,DIGITS1,AXESH1)
+%  Get show equation string
+%  FITTYPE1:  type of fit
+%  COEFFS1:  coefficients
+%  DIGITS1:  number of significant digits
+%  AXESH1:  axes
+
+if isequal(fittype1, 0)
+    s1 = 'Cubic spline interpolant';
+elseif isequal(fittype1, 1)
+    s1 = 'Shape-preserving interpolant';
+else
+    op = '+-';
+    format1 = ['%s %0.',num2str(digits1),'g*x^{%s} %s'];
+    format2 = ['%s %0.',num2str(digits1),'g'];
+    xl = get(axesh1, 'xlim');
+    fit =  fittype1 - 1;
+    s1 = sprintf('y =');
+    th = text(xl*[.95;.05],1,s1,'parent',axesh1, 'vis','off');
+    if abs(coeffs1(1) < 0)
+        s1 = [s1 ' -'];
+    end
+    for i = 1:fit
+        sl = length(s1);
+        if ~isequal(coeffs1(i),0) % if exactly zero, skip it
+            s1 = sprintf(format1,s1,abs(coeffs1(i)),num2str(fit+1-i), op((coeffs1(i+1)<0)+1));
+        end
+        if (i==fit) && ~isequal(coeffs1(i),0)
+            s1(end-5:end-2) = []; % change x^1 to x.
+        end
+        set(th,'string',s1);
+        et = get(th,'extent');
+        if et(1)+et(3) > xl(2)
+            s1 = [s1(1:sl) sprintf('\n     ') s1(sl+1:end)];
+        end
+    end
+    if ~isequal(coeffs1(fit+1),0)
+        sl = length(s1);
+        s1 = sprintf(format2,s1,abs(coeffs1(fit+1)));
+        set(th,'string',s1);
+        et = get(th,'extent');
+        if et(1)+et(3) > xl(2)
+            s1 = [s1(1:sl) sprintf('\n     ') s1(sl+1:end)];
+        end
+    end
+    delete(th);
+    % Delete last "+"
+    if isequal(s1(end),'+')
+        s1(end-1:end) = []; % There is always a space before the +.
+    end
+    if length(s1) == 3
+        s1 = sprintf(format2,s1,0);
+    end
+end
 
 
 
